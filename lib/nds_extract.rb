230d2ce947063e4fd7267590442081cf5d698a2a
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 20:41:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,8 +14,8 @@ end
 # using director_data as input
 def gross_for_director(director_data)
 
-  pp director_data.length
-  
+  pp director_data[:name]
+
   # i = 0
   # while i < director_data.length
   #   pp director_data
