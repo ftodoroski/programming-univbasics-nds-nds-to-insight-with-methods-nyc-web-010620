c1ff52e4b1d52ae1d135444df42c16bbd8263162
Filip Todoroski <ftodoroski@ymail.com>
2019-11-19 19:55:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,7 +17,8 @@ def gross_for_director(director_data)
 
   i = 0
   while i < director_data.length
-    worldwide_grosses = director_data[i][:worldwide_grosses]
+    worldwide_grosses = director_data[:movies
+      ][i][:worldwide_grosses]
 
     total_gross += worldwide_grosses
 
